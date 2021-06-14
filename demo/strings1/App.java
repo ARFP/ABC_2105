6228b674c4efc8d49261634a303c329e2aef1955
mdevoldere <mdevoldere@users.noreply.github.com>
2021-06-14 10:43:41 +0200

    Update App.java

--- demo/strings1/App.java
+++ demo/strings1/App.java
@@ -7,30 +7,47 @@ public class App
     
     public static void main(String[] args) 
     {
-        Scanner sc;
-        String saisie;
-        String[] chaine;
+        String saisie; // saisie utilisateur
+        String[] tableau; // tableau pour découper la saisie utilisateur
 
-        sc = new Scanner(System.in);
+        int valeur; // valeur à convertir
 
+        saisie = "14 km"; 
 
-        saisie = sc.nextLine(); // peut recevoir "toto" ou un nombre
-
-        if(saisie == "toto") {
-            // on quitte le programme
-            
+        if(saisie.toLowerCase().equals("quitter")) {
+            System.exit(0); // quitter le programme
         }
 
-        chaine = saisie.split(" ");
+        tableau = saisie.split(" "); // découpage de la chaine. l'espace " " sera notre séparateur
 
+        System.out.println(tableau[0]); // "14"
+        System.out.println(tableau[1]); // "km"
 
-        int valeur = Integer.parseInt(chaine[0]);
-        // 14 km
-        // 14 mi
-        // 14
+        valeur = Integer.parseInt(tableau[0]); // conversion de la chaine "14" en nombre entier 14
 
-        System.out.println(valeur  + " fin");
+        valeur = valeur *2; // on peut maintenant utiliser la valeur pour un calcul
 
-        sc.close();
+        System.out.println(valeur);
+
+       do 
+       {
+            String a;
+            int b = 0;
+            do 
+            {
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Saisissez une valeur supérieure à 0");
+                a = scanner.nextLine();
+
+                if(a.equals("q")) {
+                    System.exit(0);
+                }
+
+                b = Integer.parseInt(a);
+            } while (b < 1);
+
+            
+
+       } while (true);
     }
 }
