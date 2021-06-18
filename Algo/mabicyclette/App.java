b4ea33877b5cecc569352a9ff7a00b3b51ce82d9
mdevoldere <mdevoldere@users.noreply.github.com>
2021-06-18 11:36:52 +0200

    bibli

---
+++ Algo/mabicyclette/App.java
@@ -0,0 +1,15 @@
+package mabicyclette;
+
+
+public class App 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Ma Bicyclette !");      
+
+        System.out.println("Fin du programme");
+
+       
+
+    }
+}
