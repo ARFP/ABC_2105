c76c94e4ef23910eb83f56b87e2ac2b1e94d05fb
mdevoldere <mdevoldere@users.noreply.github.com>
2021-06-22 16:22:55 +0200

    add person

---
+++ Objet/introduction/App.java
@@ -0,0 +1,9 @@
+package introduction;
+
+public class App {
+    
+    public static void main(String[] args) 
+    {
+
+    }
+}
---
+++ Objet/introduction/Person.java
@@ -0,0 +1,11 @@
+package introduction;
+
+public class Person 
+{
+    private StringBuilder str;
+    
+    public Person() 
+    {
+        this.str = new StringBuilder();
+    }
+}
